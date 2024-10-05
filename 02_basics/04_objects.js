6eeb895ad3fc395de9b97346ab282e8e24cc03f9
srishtiv03 <155290069+srishtiv03@users.noreply.github.com>
2024-10-05 04:51:30 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,3 +22,52 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userfullname.firstname)
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {2: "a", 3: "b"}
+const obj4 = {4: "c", 5: "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign(obj1, obj2) // assign method combines obj1 and obj2 and stores it into obj1
+// console.log(obj3)
+// console.log(obj1)
+// // both will give same output
+// console.log(obj1 == obj3) // true
+// OR 
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // {} this empty object is now the target object and obj1, obj2 and obj4 are stored in {}
+// console.log(obj3)
+
+// spread method
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3)
+
+// from database the data is received in the form of arrays
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+
+// how to access?
+// console.log(users[1].email)
+
+// console.log(tinderUser)
+// { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+console.log(Object.keys(tinderUser)) // array of keys
+// [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)) // array of values
+// [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser)) // array of arrays of entires
+// [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // true
+console.log(tinderUser.hasOwnProperty('isLogged')) // false
