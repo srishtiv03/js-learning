8e2e7721f0500b2be87ca25a5e8c9a4c6ffc7549
srishtiv03 <155290069+srishtiv03@users.noreply.github.com>
2024-10-15 13:04:43 +0000

    arrow function and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,16 +8,25 @@ const user = {
     }
 }
 
+// this keyword - tells about the current context
+
 // user.welcomeMessage()
 // user.username = "sam"
 // user.welcomeMessage()
 
 // console.log(this) // {}
+// **global object in the browser - Window**
 
-// function chai(){
+// function chai(){ // 1
+//     console.log(this)
+// }
+
+// chai()
+
+// function chai(){ // 2
 //     let username = "srishti"
 //     console.log(this.username)
-// }
+// } // this objects ke andar ki kaam kar paa rha hai, aise functions ke andar use nhi kar paa rhe hai (compare 1 and 2)
 
 // chai() // undefined
 
@@ -26,21 +35,25 @@ const user = {
 //     console.log(this.username)
 // }
 
-const chai = () => {
-    let username = "srishti"
-    console.log(this)
-}
+// const chai = () => {
+//     let username = "srishti"
+//     console.log(this)
+// }
 
 // chai()
 
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
+//     return num1 + num2 // using the return keyword - EXPLICIT RETURN
 // }
 
-// const addTwo = (num1, num2) => num1 + num2 
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1, num2) => num1 + num2 // IMPLICIT RETURN - no need to use the return keyword and written without using the curly braces
+
+// console.log(addTwo(3,4))
 
 // const addTwo = (num1, num2) => (num1 + num2)
 
-const addTwo = (num1, num2) => ({username: "srishti"})
+const addTwo = (num1, num2) => ({username: "srishti"}) // returning an object, to return an object we have to write the object inside ()
 
 console.log(addTwo(3,4))
